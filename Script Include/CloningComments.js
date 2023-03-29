6f0be6f359d93cde19d436b8f93691803e648c92
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2023-03-29 17:46:00 +0500

    script for cloning comments

---
+++ Script Include/CloningComments.js
@@ -0,0 +1,38 @@
+// cloning comments
+var grAudit = new GlideRecord("sys_audit");
+grAudit.addEncodedQuery(
+  "tablename=<TABLE>^fieldname=comments^documentkey=" +
+    ogDataSetSysId
+);
+grAudit.query();
+
+if (grAudit.getRowCount() > 0) {
+  var comms = "";
+
+  while (grAudit.next()) {
+    var user = grAudit.getValue("user");
+    var comment = grAudit.getValue("newvalue");
+
+    if (user != "system") {
+      var grUser = new GlideRecord("sys_user");
+      grUser.addEncodedQuery("user_name=" + user);
+      grUser.query();
+      grUser.next();
+
+      comms +=
+        grUser.getValue("name") +
+        ":  " +
+        comment +
+        " (" +
+        grAudit.sys_created_on +
+        ")" +
+        "\n";
+    } else {
+      comms += comment;
+    }
+  }
+  record.comments = comms;
+}
+
+record.state = "draft";
+record.update();
