3f3f061715a4cd53f1b43364e0527d7483706ba2
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2023-04-04 17:30:22 +0500

    ability to delete undeletable columns

--- Background Scripts/DeleteTable.js
+++ Background Scripts/DeleteTable.js
@@ -2,3 +2,7 @@ var tu = new TableUtils();
 tu.dropAndClean("<TABLE_NAME>");
 var tu = new TableUtils();
 tu.dropAndClean("np$<TABLE_NAME>");
+
+// delete column
+gs.dropColumn("tableName", "columnName");
+
