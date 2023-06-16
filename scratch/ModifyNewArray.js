c7a467ef4a9a51b0da7ce813acc21512e7e79e29
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2023-06-16 13:04:02 +0500

    sort a multi dimensional array

--- scratch/ModifyNewArray.js
+++ scratch/ModifyNewArray.js
@@ -3,3 +3,17 @@ var array = [0,1,2,3,4,5];
 var newArray = array.slice(0, 2).concat(99).concat(array.slice(2));
 console.log(newArray);
 console.log(array);
+
+var arr = [
+  [3, ["ee", 2, 323]],
+  [23, ["ee", 2, 323]],
+  [19, ["zzz", 987, 323]],
+  [0, ["e", 2, 323]],
+];
+
+// sort the array by the first element of each sub-array
+arr.sort(function(a, b) {
+    return a[0] - b[0];
+    }
+);
+console.log(arr);
