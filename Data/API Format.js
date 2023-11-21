4ba3e07d72104ea3a4bf631470d6ab1d10720acc
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2023-11-21 18:37:51 +0500

    Sample APi format

---
+++ Data/API Format.js
@@ -0,0 +1,18 @@
+var obj = {
+  order_number: "23332",
+  order_date: "2023-01-01",
+  order_status: "shipped",
+
+  order_items: [
+    {
+      item_id: "1",
+      item_name: "item 1",
+      serial_number: "7868",
+    },
+    {
+      item_id: "2",
+      item_name: "item 2",
+      serial_number: "1234",
+    },
+  ],
+};
