d3ad88b974d7a04e9eeaf4eea404c5bee01e362f
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2024-01-25 04:17:41 +0500

    Log and create Record

--- UI Builder/Client Script/LogTypeHeadMulti.js
+++ UI Builder/Client Script/LogTypeHeadMulti.js
@@ -1,37 +1,54 @@
 /**
-* @param {params} params
-* @param {api} params.api
-* @param {any} params.event
-* @param {any} params.imports
-* @param {ApiHelpers} params.helpers
-*/
-function handler({api, event, helpers, imports}) {
-    // console.log("VAlue"+JSON.stringify(api.state.programs));
-    var programsIds = api.state.programs.map(e => e.id);
-    console.log(programsIds);
+ * @param {params} params
+ * @param {api} params.api
+ * @param {any} params.event
+ * @param {any} params.imports
+ * @param {ApiHelpers} params.helpers
+ */
+function handler({ api, event, helpers, imports }) {
+  // console.log("VAlue"+JSON.stringify(api.state.programs));
+  var programsIds = api.state.programs.map((e) => e.id);
+  console.log(programsIds);
 
-    var elmId = api.state.elm;
-    console.log(elmId);
+  var elmId = api.state.elm;
+  console.log(elmId);
 
-    var dateIds = api.state.date_types.map(i => i.id);
-    console.log(dateIds);
+  var dateIds = api.state.date_types.map((i) => i.id);
+  console.log(dateIds);
 
+  var lossIds = api.state.loss_types.map((j) => j.id);
+  console.log(lossIds);
 
-    var lossIds = api.state.loss_types.map(j => j.id);
-    console.log(lossIds);
+  var timeIds = api.state.time_intervals.map((k) => k.id);
+  console.log(timeIds);
 
-    var timeIds = api.state.time_intervals.map(k => k.id);
-    console.log(timeIds);
+  var startDate = api.state.start_date;
+  console.log(startDate);
 
-    var templateFields = "programs=" +programsIds +"^loss_types=" +lossIds+"^time_intervals="+timeIds+"^elm=" +elmId+"^date_types=" +dateIds;
-    templateFields = templateFields.toString();
-    console.log(templateFields);
+  var endDate = api.state.end_date;
+  console.log(endDate);
 
-     api.data.create_record_1.execute({
-        'table': 'x_889853_demo_app_form_submitter',
-        'templateFields': templateFields,
-        'useSetDisplayValue': false
-    });
+  var templateFields =
+    "programs=" +
+    programsIds +
+    "^loss_types=" +
+    lossIds +
+    "^time_intervals=" +
+    timeIds +
+    "^elm=" +
+    elmId +
+    "^date_types=" +
+    dateIds +
+    "^start_date=" +
+    startDate +
+    "^end_date=" +
+    endDate;
+  templateFields = templateFields.toString();
+  console.log(templateFields);
 
-    
+  api.data.create_record_1.execute({
+    table: "x_889853_demo_app_form_submitter",
+    templateFields: templateFields,
+    useSetDisplayValue: false,
+  });
 }
