b7df816423374a76d87e73ed91cc9f28b2e37633
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2025-07-24 20:08:29 +0500

    Checking values exist in the choice field or not

--- Client Scripts/onChangeScriptMSPCIs.js
+++ Client Scripts/onChangeScriptMSPCIs.js
@@ -1,24 +1,44 @@
 function onChange(control, oldValue, newValue, isLoading, isTemplate) {
-  if (newValue === "") {
-    return;
-  }
+    if (newValue === '') {
+        return;
+    }
 
-  var gx = new GlideAjax("IncidentsMSPFunctionsClient");
-  gx.addParam("sysparm_name", "showOrHideMSPCis");
-  gx.addParam("sysparm_user_sys_id", newValue);
-  gx.getXMLAnswer(callbackFunction);
+    var gx = new GlideAjax("IncidentsMSPFunctionsClient");
+    gx.addParam("sysparm_name", "showOrHideMSPCis");
+    gx.addParam("sysparm_user_sys_id", newValue);
+    gx.getXMLAnswer(callbackFunction);
+
+
+
 }
 
 function callbackFunction(response) {
-  if (response !== null && response !== "null") {
-    var json = JSON.parse(response);
-    var remove = json["remove"];
-    for (var i = 0; i < remove.length; i++) {
-      g_form.removeOption("u_msp_cis", remove[i][0]);
+
+
+    if ((response !== null) && (response !== "null")) {
+        var json = JSON.parse(response);
+        var remove = json["remove"];
+        for (var i = 0; i < remove.length; i++) {
+            g_form.removeOption("u_msp_cis", remove[i][0]);
+        }
+
+
+        var values = []; //which already exist in the choice field
+        var sel = g_form.getElement('u_msp_cis');
+        for (var k = 0; k < sel.options.length; k++) {
+            if (sel.options[k].value) {
+                values.push(sel.options[k].value);
+            }
+        }
+
+
+        var keep = json["keep"]; // keep/add
+        for (var j = 0; j < keep.length; j++) {
+            if (!(values.includes(keep[j][0]))) {
+                g_form.addOption("u_msp_cis", keep[j][0], keep[j][1]);
+            }
+        }
     }
-    var keep = json["keep"]; // keep/add
-    for (var j = 0; j < keep.length; j++) {
-      g_form.addOption("u_msp_cis", keep[j][0], keep[j][1]);
-    }
-  }
+
+
 }
