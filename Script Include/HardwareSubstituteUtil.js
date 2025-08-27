c2a8621cc35fc5e2cc68432757ad004ddab309de
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2025-08-28 00:06:08 +0500

    setting up the script include

---
+++ Script Include/HardwareSubstituteUtil.js
@@ -0,0 +1,76 @@
+var HardwareSubstituteUtil = Class.create();
+HardwareSubstituteUtil.prototype = {
+  initialize: function () {},
+
+  substituteHardware: function (scTaskSysID, ritmSysID, assetSysID, modelSysID) {
+        var mod = model;
+        var ast = asset;
+        
+
+        var gr = new GlideRecord("sc_task");
+        gr.get(scTaskSysID);
+        gr.u_asset_model = mod;
+        gr.u_asset = ast;
+  
+        var g = new GlideRecord("alm_asset");
+        g.get(ast);
+
+        gr.u_asset_tag = g.asset_tag;
+        gr.u_asset_serial_number = g.serial_number;
+        gr.update();
+
+        var taskSysId = eval("task_sys_id");
+        var gr = new GlideRecord("sc_task");
+        gr.get(taskSysId);
+
+        var ritm = new GlideRecord("sc_req_item");
+        ritm.get(gr.request_item);
+        var rit = ritm.sys_id;
+
+        var rq = new GlideRecord("sc_request");
+        rq.get(gr.request);
+        var rfor = rq.u_contractor;
+
+        var asset = new GlideRecord("alm_asset");
+        asset.addQuery("request_line", rit);
+        asset.addQuery("install_status=6^substatus=reserved");
+        asset.addQuery("reserved_for", rfor);
+        asset.query();
+        if (asset.next()) {
+          asset.install_status = 6;
+          asset.substatus = "available";
+          asset.request_line = "";
+          asset.reserved_for = "";
+          asset.update();
+        }
+        var ast2 = new GlideRecord("alm_asset");
+        ast2.addQuery("sys_id", ast);
+        ast2.query();
+        if (ast2.next()) {
+          ast2.install_status = 6;
+          ast2.substatus = "reserved";
+          ast2.request_line = rit;
+          ast2.reserved_for = rfor;
+          ast2.update();
+        }
+
+
+  },
+
+  getAvailableAssets: function (stockRoom, assetModel) {
+    var availableAssets = new GlideRecord("alm_asset");
+    availableAssets.addQuery("install_status=6^substatus=available");
+    availableAssets.addQuery("stockroom", stockRoom);
+    availableAssets.addQuery("model", assetModel);
+    availableAssets.query();
+
+    var availableAssetsArr = [];
+    while (availableAssets.next()) {
+      availableAssetsArr.push(availableAssets.sys_id.toString());
+    }
+    return availableAssetsArr.toString();
+
+  },
+
+  type: "HardwareSubstituteUtil",
+};
