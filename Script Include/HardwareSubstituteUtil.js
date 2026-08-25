f91ce7a25f38ff2147511081f343f097b17d7f82
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2026-08-26 00:29:30 +0500

    Advanced Exchange Script Include

--- Script Include/HardwareSubstituteUtil.js
+++ Script Include/HardwareSubstituteUtil.js
@@ -3,19 +3,13 @@ HardwareSubstituteUtil.prototype = {
   initialize: function () {},
 
   substituteHardware: function (scTaskSysID, newAssetSysID, newModelSysID) {
+
+
+
     var scTaskGr = new GlideRecord("sc_task");
     scTaskGr.get(scTaskSysID);
 
-    scTaskGr.u_asset_model = newModelSysID;
-    scTaskGr.u_asset = newAssetSysID;
-
-    var newAssetGr = new GlideRecord("alm_asset");
-    newAssetGr.get(newAssetSysID);
-
-    scTaskGr.u_asset_tag = newAssetGr.asset_tag;
-    scTaskGr.u_asset_serial_number = newAssetGr.serial_number;
-
-    scTaskGr.update();
+    var oldAssetSysID = scTaskGr.u_asset;
 
     var ritmGr = new GlideRecord("sc_req_item");
     ritmGr.get(scTaskGr.request_item);
@@ -25,7 +19,8 @@ HardwareSubstituteUtil.prototype = {
     rqGr.get(scTaskGr.request);
     var rfor = rqGr.u_contractor;
 
-    // free the old asset
+    
+    // // free the old asset
     var asset = new GlideRecord("alm_asset");
     asset.addQuery("request_line", ritmSysID);
     asset.addQuery("install_status=6^substatus=reserved");
@@ -34,17 +29,53 @@ HardwareSubstituteUtil.prototype = {
     if (asset.next()) {
       asset.install_status = 6;
       asset.substatus = "available";
+      asset.stockRoom = gs.getProperty("clifton_park_depot_stockroom");
       asset.request_line = "";
       asset.reserved_for = "";
+      asset.u_function = "";
+      asset.assigned_to = "";
+      asset.work_notes = "Asset is freed from the request line: " + ritmSysID + " and reserved for: " + rfor + " due to hardware substitution.";
       asset.update();
     }
 
+
+    var newAssetGr = new GlideRecord("alm_asset");
+    newAssetGr.get(newAssetSysID);
+
+    
+
+    scTaskGr.u_asset_model = newModelSysID;
+    scTaskGr.u_asset = newAssetSysID;
+
+    scTaskGr.u_asset_tag = newAssetGr.asset_tag;
+    scTaskGr.u_asset_serial_number = newAssetGr.serial_number;
+
+  
     // reserve the new asset
     newAssetGr.install_status = 1; // 1 = In Use
     newAssetGr.request_line = ritmSysID;
-    newAssetGr.reserved_for = rfor;
+    newAssetGr.assigned_to = rfor;
     newAssetGr.u_function = "Dedicated Device";
     newAssetGr.update();
+
+    scTaskGr.update();
+
+    // free the old asset
+    // var asset = new GlideRecord("alm_asset");
+    // asset.get(oldAssetSysID);
+    // asset.install_status = 6;
+    // asset.substatus = "available";
+    // asset.stockRoom = gs.getProperty("clifton_park_depot_stockroom");
+    // asset.request_line = "";
+    // asset.reserved_for = "";
+    // asset.u_function = "";
+    // asset.assigned_to = "";
+    // asset.work_notes = "Asset is freed from the request line: " + ritmSysID + " and reserved for: " + rfor + " due to hardware substitution.";
+    // asset.update();
+    
+
+    
+
   },
 
   getAvailableAssets: function (stockRoom, assetModel) {
@@ -61,7 +92,7 @@ HardwareSubstituteUtil.prototype = {
     return availableAssetsArr;
   },
 
-  getAvailableAssetsBySerialNum: function (stockRoom, assetModel, serialNumber) {
+  getAvailableAssetsBySerialNum: function (stockRoom, serialNumber) {
     var availableAssets = new GlideRecord("alm_asset");
     availableAssets.addQuery("install_status=6^substatus=available");
     availableAssets.addQuery("stockroom", stockRoom);
